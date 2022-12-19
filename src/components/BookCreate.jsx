c7872277fca64f2091b413a160de75e15f29327d
Aman_Purohit <amanpurohit785@gmail.com>
2022-12-20 00:23:31 +0530

    disable button and siable adding empty space front

--- src/components/BookCreate.jsx
+++ src/components/BookCreate.jsx
@@ -12,17 +12,19 @@ const BookCreate=({onCreate})=>{
         setTitle('');
     }
 
-    const handleChange=(e)=>{
-        setTitle(e.target.value);
+    const handleTitle=(e)=>{
+        setTitle(e.target.value.replace(/^\s/,""));
     }
+
+    const disable=title.length===0;
 
     return (
         <div className="book-create">
         <h3>Add a Book</h3>
         <form onSubmit={handleSubmit}>
             <label>Title of the book</label>
-            <input className="input" type="text" value={title} onChange={handleChange}/>
-            <button className="button" >Submit</button>
+            <input className="input" type="text" value={title} onChange={handleTitle}/>
+            <button className="button" disabled={disable}>Submit</button>
         </form>
             
         </div>
